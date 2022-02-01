76ae7a3cfb53f7ebb55688cb248f39505e9bbfb5
Alina0609 <grin4enko.alina@yandex.ru>
2022-02-02 00:00:45 +0300

    first commit

---
+++ cmake-build-debug/libs/data_structures/CMakeFiles/date_structures.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/date_structures.dir/unordered_array_set/unordered_array_set.c.obj"
+  "libdate_structures.a"
+  "libdate_structures.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/date_structures.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
