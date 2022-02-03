cbbd9bf8624c5948a5ad8f84a27946d593da8663
Alina0609 <grin4enko.alina@yandex.ru>
2022-02-03 23:21:57 +0300

    refactoring / bug fix

--- libs/data_structures/vector/voidVector.h
+++ libs/data_structures/vector/voidVector.h
@@ -11,34 +11,34 @@
 #include <stdbool.h>
 #include <memory.h>
 
- typedef struct vectorVoid {
-  void * data ; // указатель на нулевой элемент вектора
+typedef struct vectorVoid {
+    void *data; // указатель на нулевой элемент вектора
 
-  size_t size ; // размер вектора
-  size_t capacity ; // вместимость вектора
-   size_t baseTypeSize ; // размер базового типа:
-   // например, если вектор хранит int -
-   // то поле baseTypeSize = sizeof(int)
-   // если вектор хранит float -
-   // то поле baseTypeSize = sizeof(float)
-   } vectorVoid ;
+    size_t size; // размер вектора
+    size_t capacity; // вместимость вектора
+    size_t baseTypeSize; // размер базового типа:
+    // например, если вектор хранит int -
+    // то поле baseTypeSize = sizeof(int)
+    // если вектор хранит float -
+    // то поле baseTypeSize = sizeof(float)
+} vectorVoid;
 
 //возвращает структуру-дескриптор вектор базового типа из n значений
- vectorVoid createVectorV(size_t n, size_t baseTypeSize);
+vectorVoid createVectorV(size_t n, size_t baseTypeSize);
 
- //изменяет количество памяти, выделенное под хранение элементов вектора
- void reserveV(vectorVoid *v, size_t newCapacity);
+//изменяет количество памяти, выделенное под хранение элементов вектора
+void reserveV(vectorVoid *v, size_t newCapacity);
 
- //освобождает память, выделенную под неиспользуемые элементы
- void shrinkToFitV(vectorVoid *v);
+//освобождает память, выделенную под неиспользуемые элементы
+void shrinkToFitV(vectorVoid *v);
 
- //удаляет элементы из контейнера, но не освобождает выделенную память
- void clearV(vectorVoid *v);
+//удаляет элементы из контейнера, но не освобождает выделенную память
+void clearV(vectorVoid *v);
 
- //освобождает память, выделенную вектору
- void deleteVectorV(vectorVoid *v);
+//освобождает память, выделенную вектору
+void deleteVectorV(vectorVoid *v);
 
- //проверяет является ли вектор пустым
+//проверяет является ли вектор пустым
 bool isEmptyV(vectorVoid *v);
 
 //проверяет является ли вектор полным
